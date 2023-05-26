be412fbc116faaa8fd84aa44abf18b9a0593abc7
Sarad Singh <theArkein.dev@gmail.com>
2023-05-26 22:01:13 +0545

    Add Input component

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,15 +1,129 @@
 import React from 'react';
+import { ColorSchemeOptions, RoundedOptions, SizeOptions } from '../../config/stitches/variants-options.types';
+import { configedStyled } from '../../config/stitches/index.config';
+import Label from '../label/label';
+import { Text } from '../text';
 
 export interface InputProps {
+   /**
+  * Label text for the input component
+  */
+   label?: string
+  /**
+  * Placeholder text for the input component
+  */
   placeholder?: string
-  value?: string
-  disabled?: boolean
-  onChange?: () => void
-  type?: string
+  /**
+   * How large should be the input component
+  */
+  size?: SizeOptions
+  /**
+   * Should the button component have rounded corners
+   */
+  rounded?: RoundedOptions
+  /**
+   * Is input value invalid
+   */
+  invalid?: boolean
+   /**
+   * If invalid input value, display error message
+   */
+   error?: string
+  /**
+   * Should the input component be lined or outlined
+   */
+   lined?: boolean
+   /**
+   * Unique id for the input element
+   */
+   id?: string
 }
 
-export const Input = ({ placeholder, value, disabled }: InputProps) => {
+const StyledInput = configedStyled("input", {
+  width: "stretch",
+  outline: "none",
+  border: "none",
+  variants:{
+    size: {
+      lg: {
+        padding: "8px 16px",
+        fontSize: "20px"
+      },
+      base: {
+        padding: "8px 16px",
+        fontSize: "16px"
+      },
+      sm: {
+        padding: "4px 8px",
+        fontSize: "12px"
+      }
+    },
+    rounded: {
+      sm: {
+        borderRadius: "$sm"
+      },
+      base: {
+        borderRadius: "$base"
+      },
+      lg: {
+        borderRadius: "$lg"
+      },
+      full: {
+        borderRadius: "$full"
+      }
+    },
+    lined: {
+      true: {
+        borderBottom: "2px solid $secondary8",
+        '&:focus': {
+          borderBottom: "2px solid $primary8",
+        },
+      },
+      false: {
+        border: "2px solid $secondary8",
+        '&:focus': {
+          border: "2px solid $primary8",
+        },
+      }
+    },
+    invalid:{
+      true: {
+        borderColor: "$danger8",
+        '&:focus': {
+          borderColor: "$danger8",
+        },
+      }
+    },
+  },
+  defaultVariants: {
+    size: "base",
+    lined: "false",
+    invalid: "false"
+  }
+})
+
+const Container = configedStyled("div", {
+  display: "flex",
+  flexDirection: "column",
+  gap: "6px"
+})
+
+
+export const Input = ({id='', label, placeholder, rounded, lined=false, invalid=false, error="Invalid input", size="base", ...props }: InputProps) => {
 	return (
-		<input placeholder={placeholder} value={value} disabled={disabled} />
-	);
+    <Container>
+      {
+        label && (
+          <Label color={"secondary"} size={size} htmlFor={id} text={label} bold />
+        )
+      }
+      <StyledInput id={id} invalid={invalid} size={size} lined={lined} type="text" rounded={rounded} placeholder={placeholder} {...props} />
+      {
+        (invalid && error) && (
+          <Text content={error} color={"danger"} size={"sm"} />
+        )
+      }
+    </Container>
+	
+		);
 };
